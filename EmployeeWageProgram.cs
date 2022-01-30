86618ddab5dc5f8b0073763ed9283039a3ecf68a
adityavinchu <88549097+adityavinchu@users.noreply.github.com>
2022-01-30 14:36:14 +0530

    Update EmployeeWageProgram.cs

--- EmployeeWageProgram.cs
+++ EmployeeWageProgram.cs
@@ -6,18 +6,19 @@ public class EmployeeWageProgram
 {
 	public void Attendence()
 	{
-        const int PRESENT = 1, ABSENT = 0, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8;
-        Random random = new Random();
-        int check = random.Next(0, 2);
-        if (check == PRESENT)
-        {
-            Console.WriteLine("Employee is Present \n");
-            int dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            Console.WriteLine("The Daily Wage Of the Employee is : {0}", dailywage);
-        }
-        if (check == ABSENT)
-        {
-            Console.WriteLine("Employee is Absent \n");
-        }
-    }
+		const int PRESENT = 1, ABSENT = 0, WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8;
+		Random random = new Random();
+		int check = random.Next(0, 2);
+		if (check == PRESENT)
+		{
+			Console.WriteLine("Employee is Present \n");
+			int dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+			Console.WriteLine("The Daily Wage Of the Employee is : {0}", dailywage);
+		}
+		if (check == ABSENT)
+		{
+			Console.WriteLine("Employee is Absent \n");
+		}
+		}
+	}
 }
